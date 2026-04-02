1a3bfca5dbb51d4eb00482273a6a323a06916def
liddell4alex <alexmixsep@gmail.com>
2026-04-02 03:04:17 -0600

    Getting request

--- Blog/Apps/PaginaWeb/views.py
+++ Blog/Apps/PaginaWeb/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 
+import requests, json
+from urllib.parse import urlencode, quote_plus
+
 # Create your views here.
 def home(request):
-    return render(request, "index.html", {})
+
+    token = "A6Utcox848aVp9tunWYvadPEc7fs41W51W5C4KUR"
+
+    # Pagínador
+    start = 0
+
+    # Almacena articulos
+    retrieved = []
+
+
+    encoded_query = urlencode({
+                               "q": "victor de la luz",
+                               "fl": "author, title, citation_count", 
+                               "start": start,
+                               "rows": 10**10
+                              })
+
+    results = requests.get(f"https://api.adsabs.harvard.edu/v1/search/query?{encoded_query}", headers = {'Authorization': 'Bearer ' + token})
+
+    data = results.json()
+
+    docs = data["response"]["docs"]
+
+    return render(request, "index.html", {
+        'docs': docs
+    })
